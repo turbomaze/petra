9333040558779c9f96c69283e4e6095f460bda43
Anthony Liu <anthonyscottliu@gmail.com>
2015-07-10 01:05:36 -0700

    Changes the tile placement system to only show users their local changes. Adds a tile recall button.

--- client/templates/gameplay/game_template.js
+++ client/templates/gameplay/game_template.js
@@ -41,6 +41,8 @@ Template.gameTemplate.helpers({
     }
 });
 
+var stage = []; //store piece placements here before sending to the server
+
 Template.gameTemplate.events({
     'click .tile-elem': function(e, tmpl) {
         var tileId = parseInt(e.target.id.split('-')[1]);
@@ -55,35 +57,78 @@ Template.gameTemplate.events({
     'click #place-btn': function(e, tmpl) {
         e.preventDefault();
 
-        Meteor.call(
-            'placeTile',
-            this._id,
-            Session.get('selected-tile'),
-            Session.get('selected-rack-letter'),
-            function(err, result) {
-                if (err) return Errors.throw(err.reason);
-
-                if (result.notInRoom) {
-                    return Errors.throw(
-                        'You\'re not in this game room.'
-                    );
-                } else if (result.invalidRackId) {
-                    return Errors.throw(
-                        'The rack item you\'ve selected '+
-                        'is invalid.'
-                    );
-                } else if (result.invalidTileId) {
-                    return Errors.throw(
-                        'The tile you\'ve selected is '+
-                        'invalid.'
-                    );
-                } else if (result.tileAlreadyFilled) {
-                    return Errors.throw(
-                        'There\'s already a letter in this '+
-                        'tile'
-                    );
-                }
+        //get the game data you need
+        var gameData = GameRooms.findOne(this._id, {
+            fields: {
+                playerRacks: 1,
+                tiles: 1
             }
-        );
+        });
+        var tiles = gameData.tiles;
+        var tileId = Session.get('selected-tile');
+        var rack = gameData.playerRacks[Meteor.userId()];
+        var rackId = Session.get('selected-rack-letter');
+
+        //some basic error checking
+        if (tileId === false) {
+            return Errors.throw('You must select a tile.');
+        }
+
+        if (rackId === false) {
+            return Errors.throw('You must select a letter on your rack.');
+        }
+
+        if (!rack[rackId].letter) {
+            return Errors.throw('There\'s no letter in that rack slot.');
+        }
+
+        if (!!tiles[tileId].letter) {
+            return Errors.throw('There\'s already a letter there.');
+        }
+
+        //update the LOCAL collection with the changes
+        tiles[tileId].letter = rack[rackId].letter;
+        rack[rackId].letter = false;
+        var propsToUpdate = {
+            tiles: tiles
+        };
+        propsToUpdate['playerRacks.'+Meteor.userId()] = rack;
+        GameRooms._collection.update(this._id, {
+            $set: propsToUpdate
+        });
+
+        //remember your changes so you can undo them later
+        stage.push([
+            Session.get('selected-tile'),
+            Session.get('selected-rack-letter')
+        ]);
+    },
+
+    'click #recall-btn': function(e, tmpl) {
+        //get the game data you need
+        var gameData = GameRooms.findOne(this._id, {
+            fields: {
+                playerRacks: 1,
+                tiles: 1
+            }
+        }); //search local collection?
+        var tiles = gameData.tiles;
+        var rack = gameData.playerRacks[Meteor.userId()];
+
+        //undo all the staged changes
+        stage.map(function(placement) {
+            rack[placement[1]].letter = tiles[placement[0]].letter;
+            tiles[placement[0]].letter = false;
+        });
+        stage = [];
+
+        //send the undone version back to the minimongo collection
+        var propsToUpdate = {
+            tiles: tiles
+        };
+        propsToUpdate['playerRacks.'+Meteor.userId()] = rack;
+        GameRooms._collection.update(this._id, {
+            $set: propsToUpdate
+        });
     }
 });
